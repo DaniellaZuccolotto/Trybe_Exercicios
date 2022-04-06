1a8c9b841e9cd770231d13724527f2d8e2deb9b7
Daniella Zuccolotto <danielaz_9@hotmail.com>
2022-04-05 22:59:34 -0300

    Exercicios/10.2

---
+++ bloco10-dia2-front-end-components-react/my-pokedex/src/components/pokedex.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+class Pokedex extends React.Component {
+  render() {
+    return (
+      <header>
+        <h1>Pokedex</h1>
+      </header>
+    )
+  }
+}
+
+export default Pokedex
